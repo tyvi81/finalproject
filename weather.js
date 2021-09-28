1ce8a01df5a725b0e379bd4355aeb6d1971de846
tyvi81 <59772466+tyvi81@users.noreply.github.com>
2021-09-28 19:09:16 -0400

    adsf

--- weather.js
+++ weather.js
@@ -2,19 +2,16 @@ const generateweather = async function (event) {
 
     event.preventDefault();
 
-
-    var rootLocation = $("#root");
-
-    
-    rootLocation.empty();
+    var rootImg = $("#root");
+    rootImg.empty();
 
     console.log("test");
-    var latitude = "" + $("#latitudeitude").val();
-    var longitude =  "" +document.getElementById("longitudeitude").value;
+    var lat = "" + $("#latitude").val();
+    var long =  "" +document.getElementById("longitude").value;
     const result = await axios({
         method: 'GET',
         url: 'https://weatherbit-v1-mashape.p.rapidapi.com/current',
-        params: {lon: longitude, latitude: latitude, units: 'imperial', lang: 'en'},
+        params: {lon: long, lat: lat, units: 'imperial', lang: 'en'},
         headers: {
           'x-rapidapi-key': '36aa2a1ac6msh0df9f4e9860a347p12561bjsn88032da9b9aa',
           'x-rapidapi-host': 'weatherbit-v1-mashape.p.rapidapi.com'
@@ -22,13 +19,13 @@ const generateweather = async function (event) {
       
     });
 
-    let arr = {};
-    arr =result.data.data;
-    console.log(arr);
-    console.log(arr[0].temp)
+    let array = {};
+    array =result.data.data;
+    console.log(array);
+    console.log(array[0].temp)
 
 
-    let location = $(`
+    let area = $(`
     <table class="table is-hoverable">
     <thead>
       <tr>
@@ -43,45 +40,45 @@ const generateweather = async function (event) {
         <td>Temperature</td>
         <td></td>
         <td></td>
-        <td> ${arr[0].temp} </td>
+        <td> ${array[0].temp} </td>
       </tr>
       <tr>
         <td>TimeZone</td>
         <td></td>
         <td></td>
-        <td>${arr[0].timezone}</td>
+        <td>${array[0].timezone}</td>
       </tr>
       <tr>
         <td>Weather</td>
         <td></td>
         <td></td>
-        <td>${arr[0].weather.description}</td>
+        <td>${array[0].weather.description}</td>
       </tr>
       <tr>
         <td>Humidity%</td>
         <td></td>
         <td></td>
-        <td>${arr[0].rh}</td>
+        <td>${array[0].rh}</td>
       </tr>
       <tr>
         <td>Cloud Coverage</td>
         <td></td>
         <td></td>
-        <td>${arr[0].clouds}</td>
+        <td>${array[0].clouds}</td>
       </tr>
     </tbody>
   </table>
     
     `);
 
-    rootLocation.append(location);
+    rootImg.append(area);
   
 }
 
 $(document).ready(function () {
 
-    let ouput = $("body");
+    let body = $("body");
 
-    output.on("click", "#submitCoord", generateweather);
+    body.on("click", "#submitCoord", generateweather);
 
 });
