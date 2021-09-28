c4c181e8769967a756694eaff6ebf20f69495594
tyvi81 <59772466+tyvi81@users.noreply.github.com>
2021-09-28 19:04:44 -0400

    adsf

--- joke.js
+++ joke.js
@@ -1,24 +1,15 @@
 const joke = document.querySelector('.joke');
-
 const btn = document.getElementById('btn');
-
 const jokeParagraph = document.querySelector('.joke p');
 
-
 btn.addEventListener('click',getRandom);
-
 getRandom();
 async function getRandom(){
-
-  const getJoke = await fetch('https://icanhazdadjoke.com/', {
-
+  const jokeRes = await fetch('https://icanhazdadjoke.com/', {
     headers: {
-
       'Accept': 'application/json'
-
     }
-
   });
-  const jeff = await getJoke.json();
-  jokeParagraph.innerHTML = jeff.joke;
+  const resJson = await jokeRes.json();
+  jokeParagraph.innerHTML = resJson.joke;
 }
