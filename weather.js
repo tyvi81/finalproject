4208f1891117f35a7182ee53ce36fd03d71e8e28
tyvi81 <59772466+tyvi81@users.noreply.github.com>
2021-10-03 15:38:17 -0400

    adsf

--- weather.js
+++ weather.js
@@ -2,16 +2,16 @@ const generateweather = async function (event) {
 
     event.preventDefault();
 
-    var rootImg = $("#root");
-    rootImg.empty();
+    var rootGet = $("#root");
+    rootGet.empty();
 
     console.log("test");
-    var lat = "" + $("#latitude").val();
-    var long =  "" +document.getElementById("longitude").value;
+    var lattitude = "" + $("#latitude").val();
+    var longitude =  "" +document.getElementById("longitude").value;
     const result = await axios({
         method: 'GET',
         url: 'https://weatherbit-v1-mashape.p.rapidapi.com/current',
-        params: {lon: long, lat: lat, units: 'imperial', lang: 'en'},
+        params: {lon: longitude, lattitude: lattitude, units: 'imperial', lang: 'en'},
         headers: {
           'x-rapidapi-key': '36aa2a1ac6msh0df9f4e9860a347p12561bjsn88032da9b9aa',
           'x-rapidapi-host': 'weatherbit-v1-mashape.p.rapidapi.com'
@@ -19,20 +19,20 @@ const generateweather = async function (event) {
       
     });
 
-    let array = {};
-    array =result.data.data;
-    console.log(array);
-    console.log(array[0].temp)
+    let arr = {};
+    arr =result.data.data;
+    console.log(arr);
+    console.log(arr[0].temp)
 
 
-    let area = $(`
+    let table = $(`
     <table class="table is-hoverable">
     <thead>
       <tr>
-        <th>Attribute</th>
+        <th>Category</th>
         <th></th>
         <th></th>
-        <th>Value</th>
+        <th>Ammount</th>
       </tr>
     </thead>
     <tbody>
@@ -40,38 +40,38 @@ const generateweather = async function (event) {
         <td>Temperature</td>
         <td></td>
         <td></td>
-        <td> ${array[0].temp} </td>
+        <td> ${arr[0].temp} </td>
       </tr>
       <tr>
         <td>TimeZone</td>
         <td></td>
         <td></td>
-        <td>${array[0].timezone}</td>
+        <td>${arr[0].timezone}</td>
       </tr>
       <tr>
         <td>Weather</td>
         <td></td>
         <td></td>
-        <td>${array[0].weather.description}</td>
+        <td>${arr[0].weather.description}</td>
       </tr>
       <tr>
-        <td>Humidity%</td>
+        <td>Humidity Percentage</td>
         <td></td>
         <td></td>
-        <td>${array[0].rh}</td>
+        <td>${arr[0].rh}</td>
       </tr>
       <tr>
         <td>Cloud Coverage</td>
         <td></td>
         <td></td>
-        <td>${array[0].clouds}</td>
+        <td>${arr[0].clouds}</td>
       </tr>
     </tbody>
   </table>
     
     `);
 
-    rootImg.append(area);
+    rootGet.append(table);
   
 }
 
